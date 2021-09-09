6a0e096cba1e74f5820d3ac251d8bf74e777322e
MNavarro1997 <mnavarro@gteam.es>
2021-09-09 17:34:04 +0200

    Agregar archivos de proyecto.

---
+++ Recursividad/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RecursividadNombre
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba una palabra: ");
+            string palabra = Console.ReadLine();
+            Console.WriteLine("Su palabra invertida es: ");
+            Console.WriteLine(Invertir(palabra));
+        }
+        public static string Invertir(string palabra)
+        {
+            if(palabra.Length<= 1)
+            {
+                return palabra;
+            }
+            //la primera letra del array
+            char primeraLetra = palabra[0];
+            //substring--> empieza por la posicion especificada y acaba cuando termina la string.
+            string total = palabra.Substring(1);
+            return Invertir(total) + primeraLetra;
+        }
+    }
+       
+    }
+        
+
